6c0a590b8cc366161e5ce6c1d7a9907eff4de9d8
BHDHakim <hakim.bhd@gmail.com>
2022-03-09 10:08:02 +0100

    update: main app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import { getTodos } from "./services/todoApi";
+import TodoLayout from "./components/layouts/TodoLayout";
+import AddTodoDialog from "./components/AddTodoDialog";
+import TodoList from "./components/TodoList";
+import { Button } from "./components/elements";
+import { ToastProvider } from "./contexts/ToastContext";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState(null);
+  const [addTodoDialogVisible, setAddTodoDialogVisible] = useState(false);
+
+  useEffect(() => {
+    getTodos().then(({ data }) => setTodos(data));
+  }, []);
+
   return (
     <div className="App">
-    </div >
+      <ToastProvider>
+        <TodoLayout>
+          <Button block onClick={() => setAddTodoDialogVisible(true)}>
+            Add New Todo Item
+          </Button>
+          <TodoList items={todos} />
+        </TodoLayout>
+
+        <AddTodoDialog
+          visible={addTodoDialogVisible}
+          onClose={() => setAddTodoDialogVisible(false)}
+        />
+      </ToastProvider>
+    </div>
   );
-}
+};
 
 export default App;
