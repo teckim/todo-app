99176af8352e2c331688f7a2826f09c1a357ecf7
BHDHakim <hakim.bhd@gmail.com>
2022-03-09 09:50:17 +0100

    feature: new todo layout

---
+++ src/components/layouts/TodoLayout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoLayout = ({ children }) => {
+  return (
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">{children}</div>
+    </div>
+  );
+};
+
+export default TodoLayout;
