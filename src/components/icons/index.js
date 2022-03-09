4604ed4b8065e09144074cb5b5bae49ac7fac9fd
BHDHakim <hakim.bhd@gmail.com>
2022-03-09 09:48:37 +0100

    feature: icons

---
+++ src/components/icons/index.js
@@ -0,0 +1,13 @@
+import CloseIcon from "./CloseIcon";
+import DoneIcon from "./DoneIcon";
+import TimeIcon from "./TimeIcon";
+import EditIcon from "./EditIcon"
+import ErrorIcon from "./ErrorIcon";
+
+export {
+    CloseIcon,
+    DoneIcon,
+    TimeIcon,
+    EditIcon,
+    ErrorIcon
+}
