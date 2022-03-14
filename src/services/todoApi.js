ba5ad4d9462b58f704f941715fc02779502bf97f
BHDHakim <hakim.bhd@gmail.com>
2022-03-14 12:25:26 +0100

    update api service

--- src/services/todoApi.js
+++ src/services/todoApi.js
@@ -1,9 +1,21 @@
 import api from "../plugins/api";
 
-export const getTodos = function () {
+export const getTodos = () => {
   return api.get("todos");
 };
 
-export const postTodo = function (data) {
+export const postTodo = (data) => {
   return api.post("todos", data);
+};
+
+export const updateTodo = (data) => {
+  return api.put("todos", data);
+};
+
+export const updateTodoDone = ({ id, done }) => {
+  return api.put(`todos/${id}/done`, { done });
+};
+
+export const removeTodo = (id) => {
+  return api.delete(`todos/${id}`);
 };
