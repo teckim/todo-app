cf69e6df61579c39ea566768626917258781cfe2
BHDHakim <hakim.bhd@gmail.com>
2022-03-14 12:24:27 +0100

    new icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -3,11 +3,15 @@ import DoneIcon from "./DoneIcon";
 import TimeIcon from "./TimeIcon";
 import EditIcon from "./EditIcon"
 import ErrorIcon from "./ErrorIcon";
+import ChevronDownIcon from "./ChevronDownIcon"
+import ChevronUpIcon from "./ChevronUpIcon"
 
 export {
     CloseIcon,
     DoneIcon,
     TimeIcon,
     EditIcon,
-    ErrorIcon
+    ErrorIcon,
+    ChevronDownIcon,
+    ChevronUpIcon,
 }
